69e3b4043e3be01fa6411dae4bbcc792b9a99625
Romario Gustave <romagustave10@gmail.com>
2020-01-17 05:59:26 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,7 @@ class Game
       WIN_COMBINATIONS.detect do |winner|
         @board.cells[winner[0]] == @board.cells[winner[1]] && 
         @board.cells[winner[1]] == @board.cells[winner[2]]
-        @board.taken?(combination[0]+1)
+        @board.cells[winner[0]] == "X" || "O"
        end 
    end 
     
