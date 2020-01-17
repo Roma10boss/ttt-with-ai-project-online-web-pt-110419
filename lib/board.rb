6058b31a91591f55b287cdacd20e9109d1053cb4
Romario Gustave <romagustave10@gmail.com>
2020-01-17 04:16:45 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+class Board
+  attr_accessor :cells
+  
+  def initialize
+    reset!
+  end 
+  
+  def reset! 
+    @cells = Array.new(9," ")
+  end 
+  
+  def display 
+    puts "#{cells[0]} | #{cells[1]} | #{cells[2]}"
+    puts "___________"
+    puts "#{cells[3]} | #{cells[4]} | #{cells[5]}"
+    puts "___________"
+    puts "#{cells[6]} | #{cells[7]} | #{cells[8]}"
+  end 
+  
+  
+  
+end 
