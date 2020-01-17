a2e1e9a6648a59f49b9c84edc9820f724632785b
Romario Gustave <romagustave10@gmail.com>
2020-01-17 04:25:06 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -9,13 +9,13 @@ class Board
     @cells = Array.new(9," ")
   end 
   
-  def display 
-    puts "#{cells[0]} | #{cells[1]} | #{cells[2]}"
-    puts "___________"
-    puts "#{cells[3]} | #{cells[4]} | #{cells[5]}"
-    puts "___________"
-    puts "#{cells[6]} | #{cells[7]} | #{cells[8]}"
-  end 
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "-----------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
   
   def position(input)
     cells[input.to_i-1]
