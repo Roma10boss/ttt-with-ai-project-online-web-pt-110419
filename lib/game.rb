fafafe6c693597d2a927cbe89563a65bd36a6003
Romario Gustave <romagustave10@gmail.com>
2020-01-17 05:46:13 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -22,7 +22,16 @@ class Game
       @board.turn_count % 2 == 0 ? player_1 : player_2
     end 
     
+    def over?
+      won? || draw?
+    end 
+    
     def won?
-      
+      WIN_COMBINATIONS.detect do |winner|
+        @board.cells[winner[0]]
+    end 
+    
+    def draw?
+      @board.full? && !won?
     end 
 end 
