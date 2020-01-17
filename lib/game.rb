2b55f36ae8b34238076a959cc29cb0c8488d50f8
Romario Gustave <romagustave10@gmail.com>
2020-01-17 06:06:59 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -28,7 +28,7 @@ class Game
         @board.cells[winner[1]] == @board.cells[winner[2]] &&
         @board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O"
         
-        return winner
+        return winning_combo
         end 
        end 
        return false 
